97a43b025ecfc322c8674a4001eaf87939341306
aqiel <maqiel2600@gmail.com>
2016-10-07 11:18:56 +0700

    add repository on api-ecommerce

--- ecommerce_api/src/main/java/com/example/Repository/ListRepository.java
+++ ecommerce_api/src/main/java/com/example/Repository/ListRepository.java
@@ -1,9 +1,21 @@
 package com.example.Repository;
 
+import com.example.Core.eCommerceApi;
+import com.example.Dao.ListDao;
+
+import rx.Observable;
+
 /**
  * Created by Aqiel on 10/7/2016.
  *
  */
 
 public class ListRepository {
+    eCommerceApi meCommerceApi;
+    public ListRepository(eCommerceApi commerceApi){
+        meCommerceApi = commerceApi;
+    }
+    public Observable<ListDao> getListDao(){
+        return meCommerceApi.getApiService().getListDao();
+    }
 }
