c6342d0545a6d361aec8de469ee5755f347c48e8
aqiel <maqiel2600@gmail.com>
2016-10-07 11:01:25 +0700

    add api-ecommerce

---
+++ app/src/main/java/com/example/root/gitsecommerce/Constant/Constant.java
@@ -0,0 +1,9 @@
+package com.example.root.gitsecommerce.Constant;
+
+/**
+ * Created by Varokah on 10/7/2016.
+ */
+
+public class Constant {
+    public static final String BASE_URL = "http://dev.gits.co.id/apiecommerce/public/index.php";
+}
