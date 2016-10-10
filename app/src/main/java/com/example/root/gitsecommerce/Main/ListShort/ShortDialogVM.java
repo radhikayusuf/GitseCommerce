3f156d98adc6de2346bea561ee01aff05dc85d4c
aqiel <maqiel2600@gmail.com>
2016-10-10 10:56:13 +0700

    new update

--- app/src/main/java/com/example/root/gitsecommerce/Main/ListShort/ShortDialogVM.java
+++ app/src/main/java/com/example/root/gitsecommerce/Main/ListShort/ShortDialogVM.java
@@ -9,8 +9,8 @@ import id.gits.mvvmcore.viewmodel.GitsVM;
  * Created by Varokah on 10/7/2016.
  */
 
-public class ShortDialogVM extends GitsVM{
+public class ShortDialogVM {
     public ShortDialogVM(Context context) {
-        super(context);
+
     }
 }
